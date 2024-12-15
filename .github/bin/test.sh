f142f1edd0e282505a472d1ce53c1f3bc1157ec0
ikarigawa kazukiyo <kazukkiyo@icloud.com>
2024-12-15 12:50:22 +0900

    [fix] typo

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -3,9 +3,9 @@
 # tests.csv ファイルを読み込む
 while IFS=',' read -ra row; do
     # 1列目の標準入力を実行し、2列目の標準出力と比較する
-    if [ "$(npx ts-node problems/1.two-sum/main.ts "${row[0]}")" == "${row[1]}" ]; then
+    if [ "$(npx ts-node ./problems/1.two-sum/main.ts "${row[0]}")" == "${row[1]}" ]; then
         echo "Success: ${row[0]} -> ${row[1]}"
     else
         echo "Failure: ${row[0]} -> ${row[1]}"
     fi
-done < problems/1.two-sum/tests.csv
+done < ./problems/1.two-sum/test.csv
