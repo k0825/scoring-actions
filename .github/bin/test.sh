cf442d32bd674598236c81b74b66c000003213f7
ikarigawa kazukiyo <kazukkiyo@icloud.com>
2024-12-15 12:57:07 +0900

    [fix] 条件修正

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # tests.csv ファイルを読み込む
-while IFS=',' read -ra row; do
+while IFS=',' read -r input output; do
     # 1列目の標準入力を実行し、2列目の標準出力と比較する
-    if [ "$(npx ts-node ./problems/1.two-sum/main.ts "${row[0]}")" == "${row[1]}" ]; then
-        echo "Success: ${row[0]} -> ${row[1]}"
+    if [ "$(npx ts-node ./problems/1.two-sum/main.ts "${input}")" == "${output}" ]; then
+        echo "Success: ${input} -> ${output}"
     else
-        echo "Failure: ${row[0]} -> ${row[1]}"
+        echo "Failure: ${input} -> ${output}"
     fi
 done < ./problems/1.two-sum/test.csv
