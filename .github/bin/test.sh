edf22ed3b52b51dd98ab8ac8006e405111bb0c9f
ikarigawa kazukiyo <kazukkiyo@icloud.com>
2024-12-15 12:58:41 +0900

    [fix] 修正

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -3,7 +3,7 @@
 # tests.csv ファイルを読み込む
 while IFS=',' read -r input output; do
     # 1列目の標準入力を実行し、2列目の標準出力と比較する
-    if [ "$(npx ts-node ./problems/1.two-sum/main.ts "${input}")" == "${output}" ]; then
+    if [ "$(npx ts-node ./problems/1.two-sum/main.ts "${input}")" = "${output}" ]; then
         echo "Success: ${input} -> ${output}"
     else
         echo "Failure: ${input} -> ${output}"
